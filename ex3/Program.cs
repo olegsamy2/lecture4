c865d833c46d199e9e257d5dda52504607ab9525
OlegSam2 <doorsmania@mail.ru>
2022-08-22 16:29:29 +0300

    factorial

---
+++ ex3/Program.cs
@@ -0,0 +1,14 @@
+﻿// факториал - в нем есть рекурсия 
+
+double Factorial(int n) 
+{ 
+    if(n == 1) return 1;   // 1! = 1 
+                           // 0! = 1 
+    else return n * Factorial(n-1);
+} 
+
+for (int i = 1; i < 40; i++) 
+{
+// Console.WriteLine(Factorial(i)); // 1 * 2 * 3 = 6
+Console.WriteLine($"{i}! = {Factorial(i)}");
+}
