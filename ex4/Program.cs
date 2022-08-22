65ca5e1a3497384e061f36818905ae5b42599759
OlegSam2 <doorsmania@mail.ru>
2022-08-22 16:32:56 +0300

    Fibonacci

---
+++ ex4/Program.cs
@@ -0,0 +1,14 @@
+﻿// f(1) = 1 
+// f(2) = 1 
+// f(n) = f(n-1) + f(n-2) 
+
+double Fibonacci(int n)
+{ 
+    if(n == 1 || n == 2) return 1; 
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 50; i++)                 // посчитаем первые 50 чисел
+{ 
+    // Console.WriteLine(Fibonacci(i)); 
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
